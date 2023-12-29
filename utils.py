c3aa250a3d16da037571179cb40f17f26176c460
Da-Wei Lee <daviddwlee84@gmail.com>
2023-12-29 10:44:25 +0800

    Update appearance of code editor

--- utils.py
+++ utils.py
@@ -13,15 +13,85 @@ def get_customized_code_editor() -> CustomComponent:
     """
     keyboard_binding = st.selectbox("Keyboard Binding", ["vim", "vscode"])
 
+    # css to inject related to info bar
+    css_string = """
+    background-color: #bee1e5;
+
+    body > #root .ace-streamlit-dark~& {
+    background-color: #262830;
+    }
+
+    .ace-streamlit-dark~& span {
+    color: #fff;
+    opacity: 0.6;
+    }
+
+    span {
+    color: #000;
+    opacity: 0.5;
+    }
+
+    .code_editor-info.message {
+    width: inherit;
+    margin-right: 75px;
+    order: 2;
+    text-align: center;
+    opacity: 0;
+    transition: opacity 0.7s ease-out;
+    }
+
+    .code_editor-info.message.show {
+    opacity: 0.6;
+    }
+
+    .ace-streamlit-dark~& .code_editor-info.message.show {
+    opacity: 0.5;
+    }
+    """
+    # create info bar dictionary
+    info_bar = {
+        "name": "language info",
+        "css": css_string,
+        "style": {
+            "order": "1",
+            "display": "flex",
+            "flexDirection": "row",
+            "alignItems": "center",
+            "width": "100%",
+            "height": "2.5rem",
+            "padding": "0rem 0.75rem",
+            "borderRadius": "8px 8px 0px 0px",
+            "zIndex": "9993",
+        },
+        "info": [{"name": "python", "style": {"width": "100px"}}],
+    }
     buttons = [
         {
             "name": "Copy",
             "feather": "Copy",
             "hasText": True,
             "alwaysOn": True,
-            "commands": ["copyAll"],
-            "style": {"top": "0.46rem", "right": "0.4rem"},
+            "commands": [
+                "copyAll",
+                [
+                    "infoMessage",
+                    {
+                        "text": "Copied to clipboard!",
+                        "timeout": 2500,
+                        "classToggle": "show",
+                    },
+                ],
+            ],
+            "style": {"right": "0.4rem"},
         },
+        # {
+        #     "name": "Copy",
+        #     "feather": "Copy",
+        #     "hasText": True,
+        #     "alwaysOn": True,
+        #     "commands": ["copyAll"],
+        #     "style": {"top": "0.46rem", "right": "0.4rem"},
+        # },
         {
             "name": "Shortcuts",
             "feather": "Type",
@@ -71,7 +141,11 @@ def get_customized_code_editor() -> CustomComponent:
     ]
 
     return partial(
-        code_editor, shortcuts=keyboard_binding, height=[15, 30], buttons=buttons
+        code_editor,
+        shortcuts=keyboard_binding,
+        height=[15, 30],
+        buttons=buttons,
+        info=info_bar,
     )
 
 
