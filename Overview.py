90a84662636779ec42d1d1c8be15c7da29360f86
Da-Wei Lee <daviddwlee84@gmail.com>
2023-12-29 10:35:10 +0800

    Refactor and simplify code editor

--- Overview.py
+++ Overview.py
@@ -1,81 +1,16 @@
 import streamlit as st
-from code_editor import code_editor
-from utils import capture_output_streamlit
-import redirect
+import utils
 
 code = """
 print("Hello")
 """
 
-keyboard_binding = st.selectbox("Keyboard Binding", ["vim", "vscode"])
-
-buttons = [
-    {
-        "name": "Copy",
-        "feather": "Copy",
-        "hasText": True,
-        "alwaysOn": True,
-        "commands": ["copyAll"],
-        "style": {"top": "0.46rem", "right": "0.4rem"},
-    },
-    {
-        "name": "Shortcuts",
-        "feather": "Type",
-        "class": "shortcuts-button",
-        "hasText": True,
-        "commands": ["toggleKeyboardShortcuts"],
-        "style": {"bottom": "calc(50% + 1.75rem)", "right": "0.4rem"},
-    },
-    # {
-    #     "name": "Collapse",
-    #     "feather": "Minimize2",
-    #     "hasText": True,
-    #     "commands": [
-    #         "selectall",
-    #         "toggleSplitSelectionIntoLines",
-    #         "gotolinestart",
-    #         "gotolinestart",
-    #         "backspace",
-    #     ],
-    #     "style": {"bottom": "calc(50% - 1.25rem)", "right": "0.4rem"},
-    # },
-    # {
-    #     "name": "Save",
-    #     "feather": "Save",
-    #     "hasText": True,
-    #     "commands": ["save-state", ["response", "saved"]],
-    #     "response": "saved",
-    #     "style": {"bottom": "calc(50% - 4.25rem)", "right": "0.4rem"},
-    # },
-    {
-        "name": "Run",
-        "feather": "Play",
-        "primary": True,
-        "hasText": True,
-        "showWithIcon": True,
-        "commands": ["submit"],
-        "style": {"bottom": "0.44rem", "right": "0.4rem"},
-    },
-    {
-        "name": "Command",
-        "feather": "Terminal",
-        "primary": True,
-        "hasText": True,
-        "commands": ["openCommandPallete"],
-        "style": {"bottom": "3.5rem", "right": "0.4rem"},
-    },
-]
-
-return_value = code_editor(
-    code, shortcuts=keyboard_binding, height=[15, 30], buttons=buttons
-)
-
+code_editor = utils.get_customized_code_editor()
+return_value = code_editor(code)
 st.write(return_value)
 
 if return_value.get("type") == "submit":
     # https://realpython.com/python-exec/
-    capture_output_streamlit(exec)(return_value.get("text"))
-
-    # This is not work as expected
-    with redirect.stdout, redirect.stderr(format="markdown", to=st.sidebar):
-        exec(return_value.get("text"))
+    utils.capture_output_streamlit(exec)(return_value.get("text"))
+    output = utils.capture_output(exec)(return_value.get("text"))
+    st.write(output)
--- utils.py
+++ utils.py
@@ -1,7 +1,78 @@
 import streamlit as st
+from streamlit.components.v1.components import CustomComponent
 import contextlib
 from functools import wraps
 from io import StringIO
+from code_editor import code_editor
+from functools import partial
+
+
+def get_customized_code_editor() -> CustomComponent:
+    """
+    https://code-editor-documentation.streamlit.app/Advanced_usage
+    """
+    keyboard_binding = st.selectbox("Keyboard Binding", ["vim", "vscode"])
+
+    buttons = [
+        {
+            "name": "Copy",
+            "feather": "Copy",
+            "hasText": True,
+            "alwaysOn": True,
+            "commands": ["copyAll"],
+            "style": {"top": "0.46rem", "right": "0.4rem"},
+        },
+        {
+            "name": "Shortcuts",
+            "feather": "Type",
+            "class": "shortcuts-button",
+            "hasText": True,
+            "commands": ["toggleKeyboardShortcuts"],
+            "style": {"bottom": "calc(50% + 1.75rem)", "right": "0.4rem"},
+        },
+        # {
+        #     "name": "Collapse",
+        #     "feather": "Minimize2",
+        #     "hasText": True,
+        #     "commands": [
+        #         "selectall",
+        #         "toggleSplitSelectionIntoLines",
+        #         "gotolinestart",
+        #         "gotolinestart",
+        #         "backspace",
+        #     ],
+        #     "style": {"bottom": "calc(50% - 1.25rem)", "right": "0.4rem"},
+        # },
+        # {
+        #     "name": "Save",
+        #     "feather": "Save",
+        #     "hasText": True,
+        #     "commands": ["save-state", ["response", "saved"]],
+        #     "response": "saved",
+        #     "style": {"bottom": "calc(50% - 4.25rem)", "right": "0.4rem"},
+        # },
+        {
+            "name": "Run",
+            "feather": "Play",
+            "primary": True,
+            "hasText": True,
+            "showWithIcon": True,
+            "commands": ["submit"],
+            "style": {"bottom": "0.44rem", "right": "0.4rem"},
+        },
+        {
+            "name": "Command",
+            "feather": "Terminal",
+            "primary": True,
+            "hasText": True,
+            "commands": ["openCommandPallete"],
+            "style": {"bottom": "3.5rem", "right": "0.4rem"},
+        },
+    ]
+
+    return partial(
+        code_editor, shortcuts=keyboard_binding, height=[15, 30], buttons=buttons
+    )
 
 
 def capture_output_streamlit(func):
@@ -18,7 +89,7 @@ def capture_output_streamlit(func):
             with contextlib.redirect_stdout(stdout), contextlib.redirect_stderr(stderr):
                 return func(*args, **kwargs)
         except Exception as err:
-            st.write(f"Failure while executing: {err}")
+            st.error(f"Failure while executing: {err}")
         finally:
             if _stdout := stdout.getvalue():
                 st.write("Execution stdout:")
@@ -64,6 +135,7 @@ def code_interpreter_stdout(code: str) -> str:
 
 
 # TODO: code interpreter that can get the latest variable or output
+
 
 if __name__ == "__main__":
     return_value = code_interpreter_stdout("print('hello')")
